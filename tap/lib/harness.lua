65a18abc3be06927e751080f3103317718197753
PHO <pho@cielonegro.org>
2018-12-09 09:16:24 +0900

    tap/harness: Use ansi-terminal instead of using term API directly

--- tap/lib/harness.lua
+++ tap/lib/harness.lua
@@ -1,99 +1,96 @@
+local ansi       = require("ansi-terminal")
 local event      = require("event")
 local filesystem = require("filesystem")
 local parser     = require("tap/parser")
 local statistics = require("tap/statistics")
-local term       = require("term")
 local harness = {}
 harness.__index = harness
 
 function harness.new(opts)
-   local self = setmetatable({}, harness)
-   opts = opts or {}
+    local self = setmetatable({}, harness)
+    opts = opts or {}
 
-   self.verbosity = opts.verbosity or 0 -- -3 - 1
-   self.isTTY     = io.output().tty
+    self.verbosity = opts.verbosity or 0 -- -3 - 1
+    self.isTTY     = ansi.supportsANSI(io.stdout)
 
-   return self
+    return self
 end
 
 -- Run TAP producers (i.e. tests) with statistics
 function harness:runTests(files) -- [file]
-   local stats = statistics.new()
+    local stats = statistics.new()
 
-   -- Install an interrupt handler so users can terminate tests. It
-   -- will also print out statistics even though it would only be
-   -- partial.
-   local function interrupted()
-      error("FIXME: interrupted")
-   end
-   event.listen("interrupted", interrupted)
+    -- Install an interrupt handler so users can terminate tests. It
+    -- will also print out statistics even though it would only be
+    -- partial.
+    local function interrupted()
+        error("FIXME: interrupted")
+    end
+    event.listen("interrupted", interrupted)
 
-   for _, file in ipairs(files) do
-      if filesystem.exists(file) then
-         self:_runTest(stats, file)
-      else
-         io.stderr:write(file..": file not found")
-      end
-   end
-   self:_write(-2, io.stdout, stats:result())
+    for _, file in ipairs(files) do
+        if filesystem.exists(file) then
+            self:_runTest(stats, file)
+        else
+            io.stderr:write(file..": file not found")
+        end
+    end
+    self:_write(-2, io.stdout, stats:result())
 
-   event.ignore("interrupted", interrupted)
+    event.ignore("interrupted", interrupted)
 end
 
 function harness:_runTest(stats, file)
-   local pipe = io.popen(file, "r")
+    local pipe = io.popen(file, "r")
 
-   self:_write(-1, io.stdout, file.." ... ")
+    self:_write(-1, io.stdout, file.." ... ")
 
-   for r in parser(pipe) do
-      self:_write(1, io.stdout, r:tostring())
+    for r in parser(pipe) do
+        self:_write(1, io.stdout, r:tostring())
 
-      if r:isPlan() then
-         if r:hasSkip() then
-            stats:plan(file, r:testsPlanned(), r:reason())
-         else
-            stats:plan(file, r:testsPlanned(), nil)
-         end
+        if r:isPlan() then
+            if r:hasSkip() then
+                stats:plan(file, r:testsPlanned(), r:reason())
+            else
+                stats:plan(file, r:testsPlanned(), nil)
+            end
 
-      elseif r:isTest() then
-         if r:hasSkip() then
-            stats:test(file, r:isOK(), r:number(), r:description(), r:reason(), nil)
+        elseif r:isTest() then
+            if r:hasSkip() then
+                stats:test(file, r:isOK(), r:number(), r:description(), r:reason(), nil)
 
-         elseif r:hasTodo() then
-            stats:test(file, r:isOK(), r:number(), r:description(), nil, r:reason())
+            elseif r:hasTodo() then
+                stats:test(file, r:isOK(), r:number(), r:description(), nil, r:reason())
 
-         else
-            stats:test(file, r:isOK(), r:number(), r:description(), nil, nil)
-         end
+            else
+                stats:test(file, r:isOK(), r:number(), r:description(), nil, nil)
+            end
 
-         if self.verbosity == 0 and self.isTTY then
-            local savedCursX, savedCursY = term.getCursor()
-            term.write(stats:progress(file), true) -- wrap
-            term.setCursor(savedCursX, savedCursY)
-         end
+            if self.verbosity == 0 and self.isTTY then
+                ansi.saveCursor(io.stdout)
+                io.stdout:write(stats:progress(file))
+                ansi.restoreCursor(io.stdout)
+            end
 
-      elseif r:isBailOut() then
-         stats:bailOut(file, r:reason())
+        elseif r:isBailOut() then
+            stats:bailOut(file, r:reason())
+            self:_write(0, io.stdout, r:tostring())
+        end
+    end
+    stats:finished(file)
+    io.close(pipe)
 
-         if self.verbosity == 0 and self.isTTY then
-            io.stdout:write(r:tostring())
-         end
-      end
-   end
-   stats:finished(file)
-   io.close(pipe)
-
-   if stats:isOK(file) then
-      self:_write(-1, io.stdout, stats:progress(file).." ok\n")
-   else
-      self:_write(-1, io.stdout, stats:progress(file).." failed\n")
-   end
+    if stats:isOK(file) then
+        self:_write(-1, io.stdout, stats:progress(file).." ok\n")
+    else
+        self:_write(-1, io.stdout, stats:progress(file).." failed\n")
+    end
 end
 
 function harness:_write(threashold, buffer, message)
-   if self.verbosity >= threashold then
-      buffer:write(message)
-   end
+    if self.verbosity >= threashold then
+        buffer:write(message)
+    end
 end
 
 return harness
